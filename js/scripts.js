3b8e180293ff3103fb653c614bf3578f8fc56b19
goapril <77327259+goapril@users.noreply.github.com>
2021-02-16 13:00:08 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-alert('Hello world');
-
-let favoriteFood = 'Anything Korean';
-document.write(favoriteFood);
+let pokemonList = [
+    { name: 'Bulbasaur', height: 2, types: ['grass', 'poision'] },
+    { name: 'Butterfree', height: 1.1, types: ['bug', 'flying'] },
+    { name: 'Pidgeot', height: 1.5, types: ['flying', 'normal'] }
+  ];
