1349a770a1f8aa49081e04620faa5265953ea35f
goapril <77327259+goapril@users.noreply.github.com>
2021-02-18 10:42:00 +0100

    Iterate pokemonList names
    Used a for loop that iterates items in pokemonList.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,28 @@
 let pokemonList = [
-    { name: 'Bulbasaur', height: 2, types: ['grass', 'poision'] },
-    { name: 'Butterfree', height: 1.1, types: ['bug', 'flying'] },
-    { name: 'Pidgeot', height: 1.5, types: ['flying', 'normal'] }
-  ];
+  {
+    name: 'Bulbasaur',
+    height: 2,
+    types: ['grass', 'poision']
+  },
+  {
+    name: 'Butterfree',
+    height: 1.1,
+    types: ['bug', 'flying']
+  },
+  {
+    name: 'Pidgeot',
+    height: 0.5,
+    types: ['flying', 'normal']
+  }
+];
+
+// A loop that iterates over each item in pokemonList
+for (let i=0; i < pokemonList.length; i++){
+  if (pokemonList[i].height === 2) {
+    document.write(`${(pokemonList[i].name)} (height: ${pokemonList[i].height}m) - Wow! That is tall! <br/> <br/>`);
+  } else if (pokemonList[i].height > 1 && pokemonList[i].height < 2) {
+    document.write(`${(pokemonList[i].name)} (height: ${pokemonList[i].height}m) - That is an average size. <br/> <br/>`);
+  } else {
+    document.write(`${(pokemonList[i].name)} (height: ${pokemonList[i].height}m) - That is short. <br/> <br/>`);
+  }
+};
