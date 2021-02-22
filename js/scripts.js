b57638818671ebc3b052c1a770ffce5f826efd35
goapril <77327259+goapril@users.noreply.github.com>
2021-02-22 13:28:10 +0100

    Iterate using forEach function

--- js/scripts.js
+++ js/scripts.js
@@ -16,6 +16,13 @@ let pokemonList = [
   }
 ];
 
+// Use a forEach() function instead of the for loop you have to iterate over the Pokémon in your pokemonList array in order to print the details of each one.
+
+function myLoopFunction(pokemon) {
+  document.write(pokemon.name + ' is ' + pokemon.height + 'm tall. <br/> <br/>');
+}
+pokemonList.forEach(myLoopFunction);
+
 // A loop that iterates over each item in pokemonList
 for (let i=0; i < pokemonList.length; i++){
   if (pokemonList[i].height === 2) {
