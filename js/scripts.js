95c1567f9f7b08624f52b7c86726781f2c2a9baa
goapril <77327259+goapril@users.noreply.github.com>
2021-02-22 16:30:19 +0100

    Added new pokemonRepository IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -16,15 +16,49 @@ let pokemonList = [
   }
 ];
 
-// Use a forEach() function instead of the for loop you have to iterate over the Pokémon in your pokemonList array in order to print the details of each one.
+// forEach() function to iterate over the Pokémon in pokemonList array in order to print the details of each one.
 
 function myLoopFunction(pokemon) {
-  document.write(pokemon.name + ' is ' + pokemon.height + 'm tall. <br/> <br/>');
+  console.log(pokemon.name + ' is ' + pokemon.height + 'm tall. <br/> <br/>');
 }
 pokemonList.forEach(myLoopFunction);
 
+// New pokemonRepository IIFE
+let pokemonRepository = (function () {
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+
+pokemonRepository.add({ name: 'Kakuna', height: 0.6, types:['bug', 'poison'] });
+console.log(pokemonRepository.getAll());
+
+// IIFE function with loop to pokemonList array and display it in DOM
+(function () {
+  pokemonList.forEach(function(pokemon) {
+    let pokemonName = pokemon.name
+    let pokemonHeight = pokemon.height
+
+    if(pokemonHeight === 2) {
+      document.write(pokemonName + ' (height : ' + pokemonHeight + ')' + ' - Wow! That is tall! <br/> <br/>');
+    } else if(pokemonHeight > 1 && pokemonHeight < 2) {
+      document.write(pokemonName +  ' (height : ' + pokemonHeight + ')' + ' - That is an average size. <br/> <br/>');
+    } else
+      document.write(pokemonName +  ' (height : ' + pokemonHeight + ')'  + ' - That is short. <br/> <br/> ');
+    });
+})();
+
 // A loop that iterates over each item in pokemonList
-for (let i=0; i < pokemonList.length; i++){
+/* for (let i=0; i < pokemonList.length; i++){
   if (pokemonList[i].height === 2) {
     document.write(`${(pokemonList[i].name)} (height: ${pokemonList[i].height}m) - Wow! That is tall! <br/> <br/>`);
   } else if (pokemonList[i].height > 1 && pokemonList[i].height < 2) {
@@ -32,4 +66,4 @@ for (let i=0; i < pokemonList.length; i++){
   } else {
     document.write(`${(pokemonList[i].name)} (height: ${pokemonList[i].height}m) - That is short. <br/> <br/>`);
   }
-};
+}; */
