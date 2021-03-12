9ab148dc877e26efb0ea2850010327828060a7e9
april <goapril@yahoo.com>
2021-03-12 09:32:54 +0100

    Changed image UrRL and deleted old codes

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 let pokemonRepository = (function () {
   let modalContainer = document.querySelector('#modal-container');
   let pokemonList = [];
-  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=20';
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=30';
 
 // Function to get the Pokémon’s details from the server
   function showDetails(pokemon) {
@@ -41,38 +41,6 @@ let pokemonRepository = (function () {
     modal.appendChild(pokemonImage);
     modalContainer.classList.add('is-visible');
   }
-
-  // Previous code - Function for showModal to enable specifying a title and content
-  /*  function showModal(pokemon) {
-      // Clear all existing modal content
-      modalContainer.innerHTML = '';
-
-      let modal = document.createElement('div');
-      modal.classList.add('modal');
-
-      // Add the new modal content
-      let closeButtonElement = document.createElement('button');
-      closeButtonElement.classList.add('modal-close');
-      closeButtonElement.innerText = 'x';
-      closeButtonElement.addEventListener('click', hideModal);
-
-      let titleElement = document.createElement('h2');
-      titleElement.innerText = pokemon.name;
-
-      let contentElement = document.createElement('p');
-      contentElement.innerText = 'Height: ' + pokemon.height;
-
-      let pokemonImage = document.createElement('img');
-      pokemonImage.src = pokemon.imageUrl;
-
-      modal.appendChild(closeButtonElement);
-      modal.appendChild(titleElement);
-      modal.appendChild(contentElement);
-      modal.appendChild(pokemonImage);
-      modalContainer.appendChild(modal);
-
-      modalContainer.classList.add('is-visible');
-    } */
 
 // Function to close the modal
   function hideModal() {
@@ -126,8 +94,6 @@ let pokemonRepository = (function () {
     let listpokemon = document.createElement('li');
     let button = document.createElement('button');
     button.innerText = pokemon.name;
-    //button.classList.add('button-class');
-    //button.classList.add('group-list-item');
     button.classList.add('btn');
     button.classList.add('btn-outline-secondary');
     button.classList.add('btn-block');
@@ -136,7 +102,7 @@ let pokemonRepository = (function () {
     listpokemon.appendChild(button);
     pokemonList.appendChild(listpokemon);
     button.addEventListener('click', function () {
-      showDetails(pokemon /*, listpokemon*/);
+      showDetails(pokemon);
     });
   }
 
@@ -165,7 +131,7 @@ let pokemonRepository = (function () {
       return response.json();
     }).then(function (details) {
       // Now we add the details to the item
-      item.imageUrl = details.sprites.front_default;
+      item.imageUrl = details.sprites.other.dream_world.front_default;
       item.height = details.height;
       item.weight = details.weight;
       item.types = [];
@@ -215,65 +181,3 @@ pokemonRepository.loadList().then(function() {
     pokemonRepository.addListItem(pokemon);
   });
 });
-
-/* console.log('This is returned from the filter function', pokemonRepository.filterPokemon('Pidgeot', 'name'));
-pokemonRepository.add({ name: 'Kakuna', height: 0.6, types:['bug', 'poison'] });
-console.log(pokemonRepository.getAll()); */
-
-// Function to show item photo on browser when clicked in IIFE
-/*  function showDetails(pokemon, listpokemon){
-    // create Image in JavaScript
-    let imageElement = new Image();
-    imageElement.src = pokemon.imageFile;
-    listpokemon.appendChild(imageElement);
-  } */
-
-// forEach() function to iterate over the Pokémon in pokemonList array in order to print the details of each one.
-
-/* function myLoopFunction(pokemon) {
-  console.log(pokemon.name + ' is ' + pokemon.height + 'm tall. <br/> <br/>');
-}
-pokemonList.forEach(myLoopFunction); */
-
-// IIFE function with loop to pokemonList array
-/* (function () {
-  pokemonList.forEach(function(pokemon) {
-    let pokemonName = pokemon.name
-    let pokemonHeight = pokemon.height
-
-    if(pokemonHeight === 2) {
-      document.write(pokemonName + ' (height : ' + pokemonHeight + ')' + ' - Wow! That is tall! <br/> <br/>');
-    } else if(pokemonHeight > 1 && pokemonHeight < 2) {
-      document.write(pokemonName +  ' (height : ' + pokemonHeight + ')' + ' - That is an average size. <br/> <br/>');
-    } else
-      document.write(pokemonName +  ' (height : ' + pokemonHeight + ')'  + ' - That is short. <br/> <br/> ');
-    });
-})(); */
-
-// Validate whether all Object.keys() of the parameter are equal to the expected specific keys
-/* Object.keys(pokemonList).forEach(function (item) {
-	console.log(item);
-	console.log(pokemonList[item]);
-}); */
-
-// Filter array of objects with multiple values = without new repository
-/* let filteredPokemon = pokemonList.filter(function (currentElement) {
-  return currentElement.name === "Pidgeot" && currentElement.height === 0.5;
-});
-console.log(filteredPokemon);
-
-let filteredPokemon = pokemonList.filter(function (currentElement) {
-  return currentElement.height < 2 && currentElement.height > 1;
-});
-console.log(filteredPokemon); */
-
-// A loop that iterates over each item in pokemonList
-/* for (let i=0; i < pokemonList.length; i++){
-  if (pokemonList[i].height === 2) {
-    document.write(`${(pokemonList[i].name)} (height: ${pokemonList[i].height}m) - Wow! That is tall! <br/> <br/>`);
-  } else if (pokemonList[i].height > 1 && pokemonList[i].height < 2) {
-    document.write(`${(pokemonList[i].name)} (height: ${pokemonList[i].height}m) - That is an average size. <br/> <br/>`);
-  } else {
-    document.write(`${(pokemonList[i].name)} (height: ${pokemonList[i].height}m) - That is short. <br/> <br/>`);
-  }
-}; */
